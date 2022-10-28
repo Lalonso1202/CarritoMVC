a5df68405153f1dee9e82c4be21ffcab74307c76
melody-rabinovich <melodyrabinovich@gmail.com>
2022-10-28 18:50:22 -0300

    for each productos

--- CarritoMVC/wwwroot/js/site.js
+++ CarritoMVC/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+// descomentar una vez que tengamos productos para mostrar
+
+// Stock
+/*let stockProductos = [
+    { id: 1, nombre: "remera", cantidad: 1, descr: "remera basica negra", precio: 1000 }
+];
+// todavía no hace nada porque no tenemos productos. hice un array con uno solo para probar
+const contenedorProductos = document.getElementById('contenedor-productos');
+
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    // comilla invertida para escribir el html
+    div.innerHTML = ´
+    <img src=${producto.img} alt=""></img>
+    <h3>${producto.nombre}</h3>
+    <p>${producto.descr}</p>
+    <p class="precioProducto"> </p>
+
+    <button id="agregar${producto.id}" class="botonAgregar"> Agregar
+        <i class="fas-fa-shopping-cart"></i>
+     </button>
+    ´
+
+    contenedorProductos.appendChild(div);*/
+})
